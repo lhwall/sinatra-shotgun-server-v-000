af7beefbc51f1afb888fab9d32fb74c01379d9d4
Lucy Wall <lhwall@gmail.com>
2018-06-06 13:45:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Once upon a time, in a farway land, "
   end
 
 end
